d820af8403da54090538afacd5017cc079496eb3
shka88 <lky1732@gmail.com>
2020-04-08 12:21:23 +0900

    class 생성

---
+++ Robot.java
@@ -0,0 +1,23 @@
+
+/**
+ * 여기에 Robot 클래스 설명을 작성하십시오.
+ * 
+ * @author (2018315030 이가영, 2018315059 이와사키 메구미) 
+ * @version (2020.04.)
+ */
+public class Robot
+{
+    public static void main(String[] args){
+    
+    
+    
+    
+    }
+    
+    public static void RobotWlak(){
+    
+    
+    
+    
+    }
+}
