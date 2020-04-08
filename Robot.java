5023eda383fb8985590c19bae4553082da056f03
leekayon <lky1732@gmail.com>
2020-04-08 18:44:36 +0900

    scanner 작성 및 print 내용수정

--- Robot.java
+++ Robot.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * 여기에 Robot 클래스 설명을 작성하십시오.
  * 
@@ -8,16 +8,19 @@
 public class Robot
 {
     public static void main(String[] args){
-        String color;
-        int speed;
+        int speed = 5;
         
-    
+        System.out.println("교통신호의 상태는?");
+        Scanner scanner = new Scanner(System.in);
+        String color = scanner.next();
+        
+        RobotWalk(color,speed);
     
     }
     
     public static void RobotWalk(String color, double speed){
-        if(color.equals("gleen")){
-            System.out.println("속도를 유지하고 보행합니다.");
+        if(color.equals("green")){
+            System.out.println("보행속도를" + speed + "km/h를 유지하며 걷습니다.");
         }
         else if(color.equals("yellow")){
             System.out.println("보행속도를 " + speed*1.2 + "km/h로 걷습니다.");
