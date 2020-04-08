3de666d772f669187668a064f5c97ecadc579e0c
leekayon <lky1732@gmail.com>
2020-04-08 12:24:06 +0900

    변수선언

--- Robot.java
+++ Robot.java
@@ -3,13 +3,14 @@
  * 여기에 Robot 클래스 설명을 작성하십시오.
  * 
  * @author (2018315030 이가영, 2018315059 이와사키 메구미) 
- * @version (2020.04.)
+ * @version (2020.04.08)
  */
 public class Robot
 {
     public static void main(String[] args){
-    
-    
+        String color;
+        int speed;
+        
     
     
     }
