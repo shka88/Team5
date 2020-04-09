afb36f7a4108cc8e1b7b6343a518d149dccf3eaa
leekayon <lky1732@gmail.com>
2020-04-09 20:08:14 +0900

    맞춤법 변경

--- Robot.java
+++ Robot.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * 교통신호에 따라 RobotWalk 속도가 변경되는 프로그램
  * 
- * @author (2018315030 이가영, 2018315059 이와사키 메구미) 
+ * @author (2018315030 이가영,2018315059 이와사키 메구미) 
  * @version (2020.04.10)
  */
 public class Robot
