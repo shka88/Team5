9c0e8818858ec28b4700f1f48ee1a617e4c01977
iwamegu1208 <iwamegu1208@yahoo.com>
2020-04-22 14:00:31 +0900

    UnitTest 실시

--- Robot2Test.java
+++ Robot2Test.java
@@ -41,20 +41,18 @@ public class Robot2Test
     }
 
     @Test
-    public void Robot2_Test()
+    public void RobotWalk_Test()
     {
         Robot2 robot21 = new Robot2();
-        robot21.setRobotWalk("green", 5);
+        robot21.setRobotWalk("green", 5.0);
         assertEquals("green", robot21.getColor());
-        assertEquals(5.0, robot21.getSpeed(), 0.1);
-        Robot2 robot22 = new Robot2();
-        robot22.setRobotWalk("yellow", 5);
-        assertEquals("yellow", robot22.getColor());
-        assertEquals(6.0, robot22.getSpeed(), 0.1);
-        Robot2 robot23 = new Robot2();
-        robot23.setRobotWalk("red", 5);
-        assertEquals("red", robot23.getColor());
-        assertEquals(0.0, robot23.getSpeed(), 0.1);
+        assertEquals(5.0, robot21.getSpeed(), 0.0);
+        robot21.setRobotWalk("yellow", 5.0);
+        assertEquals("yellow", robot21.getColor());
+        assertEquals(6.0, robot21.getSpeed(), 0.0);
+        robot21.setRobotWalk("red", 5.0);
+        assertEquals("red", robot21.getColor());
+        assertEquals(0.0, robot21.getSpeed(), 0.0);
     }
 }
 
